031ad004b3b26e3149a67001b6c2efacbcf08c2f
Bleno <blenobok@gmail.com>
2017-04-30 01:11:39 -0300

    Add test case

--- eixo/tests.py
+++ eixo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Eixo
 
-# Create your tests here.
+class EixoTestCase(TestCase):
+    
+    def setUp(self):
+        Eixo.objects.create(eixo="Beleza")
+        Eixo.objects.create(eixo="Tecnologia")
+
+    def test_eixo_exists(self):
+        beleza = Eixo.objects.get(eixo="Beleza")
+        tecnologia = Eixo.objects.get(eixo="Tecnologia")
+        self.assertEqual(beleza.eixo, "Beleza")
+        self.assertEqual(tecnologia.eixo, "Tecnologia")
+
+
+    # def text_get_all(self):
+    #   eixos = Eixo.objects.all()
+    #   self.assertIsInstance(a, b)
