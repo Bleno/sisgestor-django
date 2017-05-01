edc82575067d245c9f1c4f5a15ac5020ed7b8766
Bleno <blenobok@gmail.com>
2017-05-01 01:25:14 -0300

    Add module unidade

--- unidade/urls.py
+++ unidade/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='unidade'),
+    url(r'^add$', views.add, name='unidade.add'),
+    url(r'^(?P<unidade_id>[0-9]+)/$', views.detail, name='unidade.detail'),
 ]
--- unidade/views.py
+++ unidade/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Unidade
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Unidade index.")
+    unidades = Unidade.objects.all()
+    return render(request, "list_unidade.html", {"unidades": unidades})
+
+def add(request):
+    if request.method == 'GET':
+        return render(request, "add_unidade.html")
+    unidade = Unidade(unidade=request.POST['unidade'], endereco=request.POST['endereco'])
+    unidade.save()
+    return HttpResponseRedirect(reverse('unidade'))
+
+def detail(request, unidade_id):
+    unidade = get_object_or_404(Unidade, pk=unidade_id)
+    context = {'unidade': unidade}
+    return render(request, "detail_unidade.html", context)
