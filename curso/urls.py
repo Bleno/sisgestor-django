56dc55db4bccd717b75b40c8257ed97af386f90b
Bleno <blenobok@gmail.com>
2017-05-04 01:59:53 -0300

    Complete form curso

--- curso/urls.py
+++ curso/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.curso, name='curso'),
+	url(r'^(?P<curso_id>[0-9]+)/$', views.detail, name='curso.detail'),
+	url(r'^add/$', views.add, name='curso.add'),
 ]
 
 
--- curso/views.py
+++ curso/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Curso
+from eixo.models import Eixo
 
 # Create your views here.
 def curso(request):
-	cursos = Curso.objects.all()
-	context = { 'cursos': cursos }
-	return render(request, 'curso.html', context)
+    cursos = Curso.objects.all()
+    context = { 'cursos': cursos }
+    return render(request, 'curso.html', context)
+
+def detail(request, curso_id):
+    curso = get_object_or_404(Curso, pk=curso_id)
+    return render(request, 'detail_curso.html', {'curso': curso})
+
+def add(request):
+    if request.method == 'GET':
+        eixos = Eixo.objects.all()
+        return render(request, 'add_curso.html', {'eixos': eixos})
+    curso = Curso(curso=request.POST['curso'],
+                  sigla=request.POST['sigla'],
+                  eixo=Eixo.objects.get(pk=request.POST['eixo'])
+                  )
+    curso.save()
+    return HttpResponseRedirect(reverse('curso'))
+
