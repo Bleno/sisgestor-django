c35f76eafc3e51afb99c84245e01881cef43aa5b
Bleno <blenobok@gmail.com>
2017-05-18 11:14:54 -0300

    Add test travis ci

--- situacao/tests.py
+++ situacao/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Situacao
 
-# Create your tests here.
+class SituacaoTestCase(TestCase):
+    
+    def setUp(self):
+        Situacao.objects.create(situacao="aposentado")
+        Situacao.objects.create(situacao="Férias")
+
+    def test_situacao_exists(self):
+        aposentado = Situacao.objects.get(situacao="aposentado")
+        ferias = Situacao.objects.get(situacao="Férias")
+        self.assertEqual(aposentado.situacao, "aposentado")
+        self.assertEqual(ferias.situacao, "Férias")
+
+    def test_view_home(self):
+        client = Client()
+        response = client.get('/situacao/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_detail(self):
+        client = Client()
+        response = client.get('/situacao/1/')
+        self.assertEqual(response.status_code, 200)
+
+    # def text_get_all(self):
+    #   situacaos = situacao.objects.all()
+    #   self.assertIsInstance(a, b)
